d06bbe4fdfe9d09522ee6e16aeef7a1bcbcda613
rayan <138983011+TRaya1n@users.noreply.github.com>
2024-01-22 18:32:20 +0000

    better API (ws->express)

--- backend/index.ts
+++ backend/index.ts
@@ -1,63 +1,84 @@
-import { WebSocket } from "ws";
+const express = require("express");
 import { Qwik } from "../src/Qwik/index";
 import serialize from "serialize-javascript";
 
 export const backend = {
   get: (client: Qwik) => {
-    const wss = new WebSocket.Server({ port: 3000 });
+    const app = express();
 
-    wss.on("connection", (ws) => {
-      ws.on("message", (data: any) => {
-        const object = data.toString("utf-8");
+    app.use(express.json());
 
-        console.log(object);
-
-        if (object.message === "<Client>#user_information") {
-          if (client.user) {
-            ws.send(
-              serialize({
-                type: 1,
-                user: {
-                  id: client.user.id,
-                  username: client.user.username,
-                  discriminator: client.user.discriminator,
-                  avatar: client.user.avatar,
-                  accentColor: client.user.accentColor,
-                  createdAt: client.user.createdAt,
-                  flags: client.user.flags?.serialize(),
-                  verified: client.user.verified,
-                  status: client.user.presence.status,
-                },
-              }),
-            );
-          } else {
-            ws.send(
-              serialize({
-                type: 1,
-                user: null,
-                message: "<Client>#user is not ready(?)",
-              }),
-            );
-          }
-        } else if (object.message === "<Client>#ws") {
-          ws.send(
-            serialize({
-              type: 2,
-              ws: {
-                ping: client.ws.ping,
-                status: client.ws.status,
-                shards: client.ws.shards,
-              },
-            }),
-          );
-        }
+    app.get("/", (req, res) => {
+      res.status(200).send({
+        message: "Working, Qwik!",
+        ws_ping: client.ws.ping,
       });
     });
 
-    wss.on("connection", (ws) => {
-      setInterval(() => {
-        ws.ping("ping-pong");
-      }, 5000);
+    app.get("/api", (req, res) => {
+      res.status(200).json({
+        p: ["/guilds/:id", "/guilds/:id/:cid"],
+        d: { i: "You can't access any data without the required header." },
+      });
     });
+
+    app.get("/api/users/:id", checkForHeader, async (req, res) => {
+      try {
+        const { id } = req.params;
+        const user = await client.users.fetch(id);
+        res.status(200).json(user.toJSON());
+      } catch (error) {
+        if (error.rawError.message === "Unknown User") {
+          return res.status(401).send({ error: "user not found" });
+        }
+      }
+    });
+
+    app.get("/api/guilds/:id", checkForHeader, async (req, res) => {
+      try {
+        const { id } = req.params;
+        const guild = await client.guilds.fetch(id);
+        res.status(200).json(guild.toJSON());
+      } catch (error) {
+        if (error.rawError.message === "Unknown Guild") {
+          return res.status(401).send({ error: "guild not found" });
+        }
+      }
+    });
+
+    app.get("/api/guilds/:id/:cid", checkForHeader, async (req, res) => {
+      try {
+        const { id, cid } = req.params;
+        const guild = await client.guilds.fetch(id);
+        const channel = await guild.channels.fetch(cid);
+        res.status(200).json(channel?.toJSON());
+      } catch (error) {
+        if (error.rawError.message === "Unknown Guild") {
+          return res.status(401).send({ error: "guild not found" });
+        } else if (error.rawError.message === "Unknown Channel") {
+          return res.status(401).send({ error: "channel not found!" });
+        }
+      }
+    });
+
+    app.listen(3000, () => {
+      console.log("Website Ready!");
+    });
+
+    function checkForHeader(req: Request, res: Response, next: any) {
+      if (
+        req.headers["x-qwik-api-key"] &&
+        req.headers["x-qwik-api-key"] === "qwik"
+      ) {
+        console.log(
+          `[${new Date().toISOString()}] ${req.method}:${req.url} / ${req.headers["x-forwarded-for"]}`,
+        );
+        return next();
+      } else {
+        return res
+          .status(401)
+          .json({ message: "Unauthorized, Missing API Key!" });
+      }
+    }
   },
 };
