950aa1bbba084bbf7274466084f0da7fa4500fa3
NotsoRay <157511314+NotsoRay@users.noreply.github.com>
2024-01-29 09:30:12 +0000

    Change log & about command changes!

--- src/config.ts
+++ src/config.ts
@@ -11,21 +11,31 @@ export const bot_config = {
 export const emojis = {
   folder: {
     id: "1200424376084992181",
-    name: "<:f_b>",
+    name: ":f_b:",
     raw: "<:f_b:1200424376084992181>",
   },
   true: {
     id: "1200466489703157831",
-    name: "<:u_t>",
+    name: ":u_t:",
     raw: "<:u_t:1200466489703157831>",
   },
   false: {
     id: "1200466558733004811",
-    name: "<:u_f>",
+    name: ":u_f:",
     raw: "<:u_f:1200466558733004811>",
   },
+
+
+  apps: {
+    github: {
+      id: '1201458525243711489',
+      name: ':a_gh:',
+      raw: '<:a_gh:1201458525243711489>'
+    }
+  }
 };
 
 export default {
+  emojis,
   bot_config,
 };
